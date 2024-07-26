7bc9274a2f88475412da78881c897a335ca1b6c7
Yehor Demenchuk <demenchuk1210m@gmail.com>
2024-07-26 15:04:33 +0200

    feat(userstat.sh): add directories info output

--- userstat.sh
+++ userstat.sh
@@ -10,6 +10,10 @@ echo Time: $(date +"%T")
 
 echo Current user: $(whoami)
 
+echo Current IP: $(ifconfig eth0 | grep 'inet ' | awk '{print $2}')
+
 echo Work time: $(uptime -p | awk -F'up ' '{print$2}')
 
 echo Current directory: $(pwd)
+
+echo -e "Accessible files:\n$(ls)"
